c0af3101c8850c8706556e47e694fa9023958440
Vyacheslav <lvv_1@list.ru>
2021-09-16 20:49:48 +0300

    test.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,3 +9,18 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+
+def test(request):
+    context = {
+        'title': 'geekshop',
+        'header': 'Welcome',
+        'user': 'Sten',
+        'products': [{'name': 'Худи черного цвета с монограммами adidas Originals', 'price':'6 090'},
+                   {'name': 'Синяя куртка The North Face', 'price': '23 725'},
+                   {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390'},
+                   {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340'},
+                   ]
+
+    }
+    return render(request, 'mainapp/test.html', context)
