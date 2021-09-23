0fc3a14bf4ddf1d006ef53091099b8f078524679
Vyacheslav <lvv_1@list.ru>
2021-09-23 23:26:01 +0300

    templates

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o777
 FILE_UPLOAD_PERMISSIONS = 0o777
+
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def login(request):
+    context = {
+        'title': 'Авторизация'
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Регистрация'
+    }
+    return render(request, 'users/register.html', context)
