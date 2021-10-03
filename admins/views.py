122885c2bc9199ed00cb832c174280de19ec1209
Vyacheslav <lvv_1@list.ru>
2021-10-03 15:45:04 +0300

    include admins + @login

---
+++ admins/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+def index(request):
+    return render(request, 'admins/admin.html')
+
+class UserListView(ListView):
+    pass
+
+class UserCreateView(CreateView):
+    pass
+
+class UserUpdateView(UpdateView):
+    pass
+
+class UserDeleteView(DeleteView):
+    pass
--- baskets/views.py
+++ baskets/views.py
@@ -27,6 +27,8 @@ def basket_remove(request, product_id):
     Basket.objects.get(id=product_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
+@login_required()
 def basket_edit(request, id, quantity):
     if request.is_ajax():
         basket = Basket.objects.get(id=id)
