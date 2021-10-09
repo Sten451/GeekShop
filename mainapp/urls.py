56b2b25c1499d83024bdf3b2d97b80b177c05a3c
Vyacheslav <lvv_1@list.ru>
2021-10-09 18:28:59 +0300

    filter ready

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -20,5 +20,6 @@ app_name = 'products'
 urlpatterns = [
 
     path('', products, name='index'),
+    path('category/<int:category_id>/', products, name='category'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -9,14 +9,15 @@ def index(request):
     return render(request, 'mainapp/index.html')
 
 
-def products(request):
+def products(request, category_id=None):
     title = "Каталог товаров"
-    all_product = Product.objects.all()
     # выводить будем только категории в которых есть товары
     product_category = ProductCategory.objects.annotate(cnt=Count('product')).filter(cnt__gt=0)
+    all_product = Product.objects.filter(category_id=category_id) if category_id != None else Product.objects.all()
     context = {
         'title': title,
         'all_product': all_product,
-        'product_category': product_category
+        'product_category': product_category,
+
     }
     return render(request, 'mainapp/products.html', context)
