fd3170a1fcafbe956cddf4ca0190a708314371ad
Vyacheslav <lvv_1@list.ru>
2021-10-23 18:12:55 +0300

    temlates

---
+++ ordersapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
+
+class OrderList(ListView):
+    pass
+
+class OrderUpdate(UpdateView):
+    pass
+
+class OrderCreate(CreateView):
+    pass
+
+class OrderDelete(DeleteView):
+    pass
+
+class OrderDetail(DetailView):
+    pass
+
+
+
+def order_forming_complete(request, pk):
+    pass
