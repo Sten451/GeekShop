3018b2d4a230dd7fb64c6d4d9ac820877024c210
Vyacheslav <lvv_1@list.ru>
2021-10-29 10:57:08 +0300

    signal

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
 
+from baskets.models import Basket
 from mainapp.models import Product
 
 
@@ -59,3 +62,18 @@ class OrderItem(models.Model):
     @staticmethod
     def get_item(pk):
         return OrderItem.objects.get(pk=pk).quantity
+
+@receiver(pre_delete, sender=Basket)
+@receiver(pre_delete, sender=OrderItem)
+def product_quantity_update_del(sender, instance,**kwargs):
+    instance.product.quantity += instance.quantity
+    instance.save()
+
+@receiver(pre_save, sender=Basket)
+@receiver(pre_save, sender=OrderItem)
+def product_quantity_update_del(sender, instance, **kwargs):
+    if instance.pk:
+        instance.product.quantity -= instance.quantity - instance.get_item(int(instance.pk))
+    else:
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
