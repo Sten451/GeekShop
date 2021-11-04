8d55657afacee4d1f57f8e4a6c8abc7af807ef6e
Vyacheslav <lvv_1@list.ru>
2021-11-04 17:07:48 +0300

    collecting

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,3 +18,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    import debug_toolbar
+
+    urlpatterns += [path(r'^__debug__/', include(debug_toolbar.urls))]
